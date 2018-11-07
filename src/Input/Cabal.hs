1b89f4854dfff831ac29dd65276bfd3487f878b9
templateK <templateK@github.com>
2018-11-08 08:48:55 +0900

    bump ghc 8.6.2 package-db

--- src/Input/Cabal.hs
+++ src/Input/Cabal.hs
@@ -82,8 +82,8 @@ readGhcPkg settings = do
     topdir  <- findExecutable "ghc-pkg"
     homeDir <- getHomeDirectory
     let ghcPkgArgs = [ "dump"
-                     , "--package-db=" ++ homeDir </> "compilers/ghc/ghc-8.6.1/lib/ghc-8.6.1/package.conf.d"
-                     , "--package-db=" ++ homeDir </> ".cabal/store/ghc-8.6.1/package.db"
+                     , "--package-db=" ++ homeDir </> "compilers/ghc/ghc-8.6.2/lib/ghc-8.6.2/package.conf.d"
+                     , "--package-db=" ++ homeDir </> ".cabal/store/ghc-8.6.2/package.db"
                      ]
     -- important to use BS process reading so it's in Binary format, see #194
     (exit, stdout, stderr) <- BS.readProcessWithExitCode "ghc-pkg" ghcPkgArgs mempty
